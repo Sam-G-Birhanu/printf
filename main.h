0106bf3cb9e23a0b991f1b6733c98fc1e0ba397a
Sam-G-Birhanu <samrawitguangulb@gmail.com>
2023-08-22 15:34:13 -0700

    changed

--- main.h
+++ main.h
@@ -3,9 +3,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
 
 int _printf(const char *format, ...);
 void printNumber(int num);
-void init_buffer(char buff_size[]);
+void init_buffer(char *buff_size[]);
 
 #endif /* MAIN_H */
