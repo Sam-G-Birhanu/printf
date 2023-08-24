65bf13a4cbe2d23684242fd9e53f78f15fbaddbc
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-08-25 02:14:13 +0530

    Update printUnsignedNumber.c

---
+++ printUnsignedNumber.c
@@ -0,0 +1,23 @@
+int printUnsignedNumber(unsigned int num, int sum)
+{
+    char digits[20];
+    int index = 0;
+    int i;
+
+    if (num == 0)
+    {
+        write(1, "0", 1);
+        sum++;
+        return sum;
+    }
+    while (num > 0)
+    {
+        digits[index] = (num % 10) + '0';
+        num /= 10;
+        index++;
+        sum++;
+    }
+    for (i = index - 1; i >= 0; i--)
+        write(1, &digits[i], 1);
+    return sum;
+}
