d3329eae6dd45907dd1f35a9e4bfd2e0707925a4
Sam-G-Birhanu <samrawitguangulb@gmail.com>
2023-08-25 03:56:24 -0700

    checked betty

--- printUnsignedNumber.c
+++ printUnsignedNumber.c
@@ -1,24 +1,31 @@
 #include "main.h"
+/**
+ * printUnsignedNumber - a function that prints an unsigned int type
+ * @num: parameter to accept the number being printed
+ * @sum: parameters representing the count
+ * Return: int
+ **/
+
 int printUnsignedNumber(unsigned int num, int sum)
 {
-    char digits[20];
-    int index = 0;
-    int i;
+	char digits[20];
+	int index = 0;
+	int i;
 
-    if (num == 0)
-    {
-        write(1, "0", 1);
-        sum++;
-        return sum;
-    }
-    while (num > 0)
-    {
-        digits[index] = (num % 10) + '0';
-        num /= 10;
-        index++;
-        sum++;
-    }
-    for (i = index - 1; i >= 0; i--)
-        write(1, &digits[i], 1);
-    return sum;
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		sum++;
+		return (sum);
+	}
+	while (num > 0)
+	{
+		digits[index] = (num % 10) + '0';
+		num /= 10;
+		index++;
+		sum++;
+	}
+	for (i = index - 1; i >= 0; i--)
+		write(1, &digits[i], 1);
+	return (sum);
 }
